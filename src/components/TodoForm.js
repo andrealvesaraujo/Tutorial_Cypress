6f059ea9f005d795c3fe92c9cb0acf93e560441b
Andre Alves <andrealvesaraujo.1995@gmail.com>
2021-12-21 10:55:11 -0300

    feature: Add text inputs tests

--- src/components/TodoForm.js
+++ src/components/TodoForm.js
@@ -4,6 +4,9 @@ export default props =>
   <form>
     <input
       type='text'
+      autoFocus
+      onChange={props.handleNewTodoChange}
       className="new-todo"
+      value={props.currentTodo}
       placeholder="What needs to be done?"/>
   </form>
